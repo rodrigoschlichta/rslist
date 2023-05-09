cec14211a428aa8e801fb36593605d99571f59e2
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2023-05-09 11:41:53 -0300

    first commit

---
+++ rslist/src/main/java/com/camfel/rslist/RslistApplication.java
@@ -0,0 +1,13 @@
+package com.camfel.rslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RslistApplication.class, args);
+	}
+
+}
---
+++ rslist/src/test/java/com/camfel/rslist/RslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camfel.rslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
