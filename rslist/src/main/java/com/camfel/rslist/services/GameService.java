e1c6d2181ae5c362e9a4e962bb99de85d7ecf331
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2023-05-11 10:59:01 -0300

    Get game by id

--- rslist/src/main/java/com/camfel/rslist/services/GameService.java
+++ rslist/src/main/java/com/camfel/rslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.camfel.rslist.dto.GameDTO;
 import com.camfel.rslist.dto.GameMinDTO;
 import com.camfel.rslist.entities.Game;
 import com.camfel.rslist.repositories.GameRepository;
@@ -15,7 +17,16 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
-
+	
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result  = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
